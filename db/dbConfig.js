9f3f75ce2b58a9e7ad54c24cdecbbd2568727151
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-05-17 22:22:57 -0400

    adding db config in db folder

---
+++ db/dbConfig.js
@@ -0,0 +1,22 @@
+const pgp = require('pg-promise')();
+
+require('dotenv').config();
+
+DATABASE_URL = process.env.DATABASE_URL
+
+const cn = DATABASE_URL ? {
+    connectionString: DATABASE_URL,
+    max:30
+} : {
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    database: process.env.PG_DATABASE,
+    user: process.env.PG_USER
+};
+
+// to connect the promise to the database connection object allows us to write SQL
+const db = pgp(cn)
+
+// Now we can Query!
+
+module.exports = db;
