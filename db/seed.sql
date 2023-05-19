6e37be607a72ce7023e89fb11b2d29ab1655fb38
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-05-19 01:54:55 -0400

    added data into seed.sql

---
+++ db/seed.sql
@@ -0,0 +1,15 @@
+INSERT INTO chores (name, description, due_date, status, points)
+VALUES
+  ('Clean Bedroom', 'Tidy up and organize the bedroom.', '2023-05-24', 'Pending', 20),
+  ('Wash the Dishes', 'Clean the dirty dishes and put them away.', '2023-05-19', 'Completed', 20),
+  ('Take out the Trash', 'Empty all the trash bins and replace the bags.', '2023-05-20', 'Overdue', 15),
+  ('Vacuum the Floor', 'Use the vacuum cleaner to clean the floors.', '2023-05-21', 'Pending', 25),
+  ('Feed the Pet', 'Give food and water to the pet.', '2023-05-22', 'Pending', 10),
+  ('Fold Laundry', 'Fold and organize clean laundry.', '2023-05-23', 'Completed', 15),
+  ('Clean Bedroom', 'Tidy up and organize the bedroom.', '2023-05-24', 'Overdue', 20),
+  ('Water Plants', 'Ensure plants receive adequate watering.', '2023-05-25', 'Completed', 10),
+  ('Set the Table', 'Set the table for meals.', '2023-05-26', 'Pending', 15),
+  ('Mow the Lawn', 'Trim and maintain the lawn.', '2023-05-27', 'Overdue', 30),
+  ('Organize Toys', 'Arrange and put away toys and games.', '2023-05-29', 'Pending', 10),
+  ('Sweep the Patio', 'Sweep and clean the outdoor patio area.', '2023-05-30', 'Pending', 15),
+  ('Empty Dishwasher', 'Unload and put away dishes from the dishwasher.', '2023-05-31', 'Completed', 20);
