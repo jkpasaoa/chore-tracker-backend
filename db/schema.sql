6dcb5f7576787f1282d3004edff3b89be6329a7d
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-05-19 01:48:42 -0400

    added schema.sql file

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+-- IF DATABASE EXISTS -- DROP IT
+DROP DATABASE IF EXISTS chores_dev;
+
+-- Create database
+CREATE DATABASE chores_dev;
+
+-- Connect to DB
+\c chores_dev;
+
+CREATE TABLE chores (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  description TEXT,
+  due_date DATE,
+  status TEXT NOT NULL,
+  points INT NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW(),
+  updated_at TIMESTAMP DEFAULT NOW()
+);
