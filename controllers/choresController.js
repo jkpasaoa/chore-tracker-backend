5a746519ec38dae0805253a5f55eab100c40233c
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-05-22 21:40:16 -0400

    change to package

--- controllers/choresController.js
+++ controllers/choresController.js
@@ -7,66 +7,60 @@ const {
   deleteChore,
   updateChore,
 } = require("../queries/chores");
-// const { checkName } = require("../validations/checkChores");
 
 //INDEX
-chores.get('/', async (req, res) => {
-  const allChores = await getAllChores();
-
-  if (allChores) {
-    res.status(202).json(allChores);
-  } else {
-    res.status(500).json({ error: 'Server Error' })
+chores.get("/", async (req, res) => {
+  try {
+    const allChores = await getAllChores();
+    res.status(200).json(allChores);
+  } catch (error) {
+    res.status(500).json({ error: "Server Error" });
   }
 });
 
 //SINGLE CHORE
-chores.get('/:id', async (req, res) => {
+chores.get("/:id", async (req, res) => {
   const { id } = req.params;
-  const chore = await getChore(id)
-
-  if (chore) {
+  try {
+    const chore = await getChore(id);
     res.status(200).json(chore);
-  } else {
-    res.status(500).json({ error: 'Server Error' })
+  } catch (error) {
+    res.status(500).json({ error: "Server Error" });
   }
-})
+});
 
 //CREATE/NEW CHORE Route
-chores.post('/', async (req, res) => {
+chores.post("/", async (req, res) => {
   const newChore = req.body;
   try {
-    const addedChore = await createChore(newChore)
-    res.status(202).json(addedChore)
+    const addedChore = await createChore(newChore);
+    res.status(201).json(addedChore);
   } catch (error) {
-    res.status(400).json({ error: error })
+    res.status(400).json({ error: error.message });
   }
-})
+});
 
 //DELETE CHORE
-chores.delete('/:id', async (req, res) => {
+chores.delete("/:id", async (req, res) => {
   const { id } = req.params;
-
   try {
     const deletedChore = await deleteChore(id);
-    res.status(200).json(deletedChore)
+    res.status(200).json(deletedChore);
   } catch (error) {
-    res.status(400).json({ error: error })
+    res.status(400).json({ error: error.message });
   }
 });
 
 //UPDATE/EDIT CHORE
-chores.put('/:id', async (req, res) => {
+chores.put("/:id", async (req, res) => {
   const { id } = req.params;
   const { body } = req;
-
   try {
     const updatedChore = await updateChore(id, body);
     res.status(200).json(updatedChore);
   } catch (error) {
-    res.status(400).json({ error: error });
-  };
+    res.status(400).json({ error: error.message });
+  }
 });
-
 
 module.exports = chores;
